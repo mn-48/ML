4e4ccb461fe07bd58bfbcddde13885f4b13c4fb4
mn-48 <nazmul.cse48@gmail.com>
2025-01-09 13:26:03 +0600

    OK

---
+++ QA_ML/ML_8_multinomial-naive-bayes.py
@@ -0,0 +1,16 @@
+class MultinomialNB:
+    def __init__(self, articles_per_tag):
+        self.alpha = 1
+        self.priors_per_tag = {}
+        self.likely
+        # Don't change the following two lines of code.
+        self.articles_per_tag = articles_per_tag  # See question prompt for details.
+        self.train()
+
+    def train(self):
+        # Write your code here.
+        pass
+
+    def predict(self, article):
+        # Write your code here.
+        pass
