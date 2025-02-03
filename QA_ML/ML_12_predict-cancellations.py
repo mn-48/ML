57d2244b09709e51cac87f42ea900bf9f8d6b33a
mn-48 <nazmul.cse48@gmail.com>
2025-02-03 11:00:07 +0600

    predict_cancellations

--- QA_ML/ML_12_predict-cancellations.py
+++ QA_ML/ML_12_predict-cancellations.py
@@ -1,20 +1,24 @@
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.feature import VectorAssembler
 
+
 def predict_cancellations(user_interaction_df):
     assembler = VectorAssembler(
         inputCols=["month_interaction_count", "week_interaction_count", "day_interaction_count"], outputCol="features"
     )
-
+    user_interaction_df.show()
     features_df = assembler.transform(user_interaction_df)
-    features_df = features_df.withColumn("label", features_df["cancelled_within_week"])
+    features_df = features_df.withColumn(
+        "label", features_df["cancelled_within_week"])
 
-    lr_model = LogisticRegression(maxIter=10, threshold=0.6, elasticNetParam=1, regParam=0.1)
+    lr_model = LogisticRegression(
+        maxIter=10, threshold=0.6, elasticNetParam=1, regParam=0.1)
 
     trained_lr_model = lr_model.fit(features_df)
 
     predicts_df = trained_lr_model.transform(features_df)
-    predicts_df = predicts_df.select(["user_id", "rawPrediction", "probability", "prediction"])
+    predicts_df = predicts_df.select(
+        ["user_id", "rawPrediction", "probability", "prediction"])
 
+    predicts_df.show()
     return predicts_df
-
