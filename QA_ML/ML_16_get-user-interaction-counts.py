8c01e8ff24d106007b266ca5495b2fc8d643d265
mn-48 <nazmul.cse48@gmail.com>
2025-02-04 17:01:09 +0600

    get_user_interaction_counts

--- QA_ML/ML_16_get-user-interaction-counts.py
+++ QA_ML/ML_16_get-user-interaction-counts.py
@@ -1,3 +1,12 @@
+from  datetime import datetime, timedelta
+
 def get_user_interaction_counts(search_interaction_df):
-    # Write your code here.
-    pass
+    latest_date_string = search_interaction_df.agg({"date": "max"}).collect()[0][0]
+    latest_date = datetime.strptime(latest_date_string, "%Y-%m-%d")
+
+    user_month_counts = get_df_counts_from_date_by_user_id(search_interaction_df, latest_date, 30)
+
+def get_df_counts_from_date_by_user_id(df, end_date, days_delta):
+    start_date = end_date - days_delta
+    return df.where(df["date"].between(start_date, end_date)).groupBy("user_id").count()
+
